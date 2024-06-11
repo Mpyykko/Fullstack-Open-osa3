00659bfc145523b67690613ea934a265ac9046e4
Mikko Pyykkönen <pyykkonenmikko@gmail.com>
2024-06-11 14:47:15 +0300

    Kolmosen aloitusta omaan repoon

---
+++ puhelinluettelo/index.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323532"
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234-345"
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122"
+  },
+  {
+    id: 5,
+    name: "Mary Poppendick",
+    number: "39-23-6423122"
+  }
+  
+]
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    console.log('x')
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
