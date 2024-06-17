641b708dfe92b6c82d1177d4ade8ac1da89cd8e6
Mikko Pyykkönen <pyykkonenmikko@gmail.com>
2024-06-17 12:57:33 +0300

    mongoa

--- puhelinluetteloback/mongo.js
+++ puhelinluetteloback/mongo.js
@@ -1,31 +1,56 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
+if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
 
 const password = process.argv[2]
 
+const name = process.argv[3]
+const number = process.argv[4]
+
+
 const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+  `mongodb+srv://fullstack:${password}@cluster0.orw1d9n.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
+  mongoose.set('strictQuery', false)
+  mongoose.connect(url)
+    .then(() => {
+      console.log('Connected to MongoDB')
+  
+      const personSchema = new mongoose.Schema({
+        name: String,
+        number: String,
+      })
+  
+      const Person = mongoose.model('Person', personSchema)
+  
+      if (process.argv.length === 3) {
+        Person.find({}).then(result => {
+          console.log('phonebook:')
+          result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+          })
+          mongoose.connection.close()
+        })
+      } else if (process.argv.length === 5) {
+        const person = new Person({
+          name: name,
+          number: number,
+        })
+  
+        person.save().then(() => {
+          console.log(`added ${name} number ${number} to phonebook`)
+          mongoose.connection.close()
+        })
+      } else {
+        mongoose.connection.close()
+      }
+    })
+    .catch((error) => {
+      mongoose.connection.close()
+    })
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
 
-const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
-})
-
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
